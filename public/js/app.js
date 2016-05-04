c0cf90b5d6f3fe195d1c9bea542d845c247d941d
aj <aj@snapzio.com>
2016-05-04 16:04:50 +0800

    add simple chat ui

--- public/js/app.js
+++ public/js/app.js
@@ -8,3 +8,15 @@ socket.on('message', function (message) {
   console.log('New Message');
   console.log(message.text);
 });
+
+var $form = jQuery('#message-form');
+
+$form.on('submit', function (event) {
+  event.preventDefault();
+
+  socket.emit('message', {
+    text: $form.find('[name=message]').val()
+  });
+
+  $form.find('[name=message]').val("");
+});
